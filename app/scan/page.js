f2ab3832a5d54462afa0806baa31e33228b79886
Paul Chidiadi <paulchidiadi@gmail.com>
2024-04-15 13:46:38 +0100

    Setup project

---
+++ app/scan/page.js
@@ -0,0 +1,14 @@
+"use client";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import React, { useState, useEffect } from "react";
+
+export default function Scan() {
+  const router = useRouter();
+
+  return (
+    <div className="splash">
+      <h3>fruitScan</h3>
+    </div>
+  );
+}
