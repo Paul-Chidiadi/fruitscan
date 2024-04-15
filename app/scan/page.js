a361008a0afb81ae0d2584a08e8b91056b692c69
Paul Chidiadi <paulchidiadi@gmail.com>
2024-04-15 19:15:03 +0100

    Build splash screen page and setup camera with button

--- app/scan/page.js
+++ app/scan/page.js
@@ -1,14 +1,60 @@
 "use client";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import React, { useState, useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 export default function Scan() {
-  const router = useRouter();
+  const [camView, setCamView] = useState("environment");
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    const startWebcam = async () => {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+          video: {
+            facingMode: { exact: camView }, // Try to use rear camera
+          },
+          audio: false,
+        });
+        videoRef.current.srcObject = stream;
+      } catch (error) {
+        console.error(error);
+        // If using rear camera fails, try using front camera
+        console.log("Attempting to use front camera...");
+        try {
+          const stream = await navigator.mediaDevices.getUserMedia({
+            video: { facingMode: "user" },
+            audio: false,
+          });
+          videoRef.current.srcObject = stream;
+        } catch (error) {
+          console.error("Error accessing camera:", error);
+          // Display error message or take appropriate action
+        }
+      }
+    };
+
+    startWebcam();
+
+    return () => {
+      // Cleanup code if needed
+    };
+  }, []);
 
   return (
-    <div className="splash">
-      <h3>fruitScan</h3>
+    <div className="cam-container">
+      <div className="top">
+        <i
+          className="bx bx-repeat"
+          onClick={() => {
+            setCamView((prev) => (prev === "environment" ? "user" : "environment"));
+          }}></i>
+      </div>
+      <video ref={videoRef} autoPlay playsInline />
+      <div className="bottom">
+        <i className="bx bx-scan"></i>
+        <p>Gallery</p>
+      </div>
     </div>
   );
 }
