e3922c1e1943fa0e83a2d8116dfa46fb13b29caa
Paul <paulchidiadi@gmail.com>
2024-06-02 21:20:22 +0100

    Update page.js

--- app/scan/page.js
+++ app/scan/page.js
@@ -12,7 +12,7 @@ export default function Scan() {
   const loadingRef = useRef(null);
   const responseRef = useRef(null);
   const fileInputRef = useRef(null);
-  const apiUrl = "http://54.236.135.145:8000/predict";
+  const apiUrl = "https://dataexpo.tech:8000/predict";
 
   const handleFileClick = () => {
     fileInputRef.current.click();
